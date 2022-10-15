862e3da027c639aaf1292c15b41ddd926632d2d2
Rumpels95 <joan.quispec@gmail.com>
2022-10-15 13:31:35 -0500

    sendgrid

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,6 +5,24 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const authConfig = require('../../config/auth');
 
+require("dotenv").config()
+
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+const sendMail = async(msg) => {
+  try{
+    await sgMail.send(msg)
+    console.log("Message send succesffully")
+  } catch (error){
+    console.error(error)
+    if(error.responde){
+      console.error(error.response.body)
+    }
+  }
+}
+
 async function signIn(req, res) {
     let { email, password } = req.body;
     User.findOne({ where: { email } }).then(user => {
@@ -38,6 +56,12 @@ async function signUp(req, res) {
       let token = jwt.sign({ user: user }, authConfig.secret, {
           expiresIn: authConfig.expires
       });
+      sendMail({
+        to: req.body.email,
+        from: "joan.quispec@gmail.com",
+        subject: "Welcome to Disney API",
+        text: "Now you can use it!"
+      })
       res.json({ user: user, token: token });
   }).catch(err => {
       res.status(500).json(err);
